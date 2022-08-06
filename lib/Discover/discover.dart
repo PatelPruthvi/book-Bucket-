298d2e2323f75d0c44c5e392762540dd0e66f1f5
Prince Patel <prince90290@gmail.com>
2022-08-06 12:48:29 +0530

    GNav_bar_added

---
+++ lib/Discover/discover.dart
@@ -0,0 +1,18 @@
+import 'package:bookbucket/Utils/utils.dart';
+import 'package:flutter/material.dart';
+
+class Discover extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: dark_blue_1,
+        elevation: 5,
+        title: Text('Discover'),
+      ),
+      body: Center(
+        child: Text('Discover'),
+      ),
+    );
+  }
+}
---
+++ lib/Utils/utils.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color app_primary_color = Color(0xFF001624);
+const Color dark_blue = Color(0xFF001F3F);
+const Color dark_blue_1 = Color(0xFF2f3e5c);
